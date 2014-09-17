409c944ccb55dab4bce02ffe48c8d9ab805819b9
bobsaggetismine <brodiequinlan1997@gmail.com>
2014-09-17 19:04:26 -0400

    Upload Program.cs for cmd

---
+++ Cmd/ConsoleApplication6/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a command: ");
+            String command = Console.ReadLine();
+            int e = command.IndexOf(" ");
+            int z = command.Length - 6;
+
+            String create = command.Substring(e, z);
+
+            
+            if (command.Substring(0, e).Equals("create"))
+            {
+                try
+                {
+                    File.Create(create);
+                    Console.WriteLine("You have success fully created :" + command.Substring(e, z));
+                }
+                catch (Exception i)
+                {
+                    Console.WriteLine("File already exist.");
+                }
+
+            }
+            else if (command.Substring(0, e).Equals("delete"))
+            {
+                File.Delete("C:\\Users\\brodie\\desktop\\"+"fuck.txt");
+            }
+            
+            
+            
+            
+            Console.Read();
+        }
+    }
+}
