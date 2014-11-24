d765a71c34c6232755191e34af39a3678c0f86ec
bobsaggetismine <brodiequinlan1997@gmail.com>
2014-11-23 22:35:55 -0500

    0.0.0.2 commented

--- crazy 8s/crazy 8s/Program.cs
+++ crazy 8s/crazy 8s/Program.cs
@@ -14,27 +14,33 @@ namespace crazy_8s
         public static string[] suitp2 = new string[8];
         static void Main(string[] args)
         {
-            
-            
+
+            //create the arrays of each suit
             int[] hearts = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, };
             int[] diamonds = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, };
             int[] clubs = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, };
             int[] spade = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, };
-
+            //create the deck
             int[][] cards = { hearts, diamonds, clubs, spade };
 
-            handoutcards(cards,player2,suitp2);
-
-
+            //calls methods to get adn output the players hand. send which array you want to modify, they are global arrays.
+            handoutcards(cards, player2, suitp2);
             sortcards(player2);
-            
-            outputcards(player2,suitp2);
+            outputcards(player2, suitp2);
 
-        
+
+            handoutcards(cards, player1, suitp1);
+            sortcards(player1);
+            outputcards(player1, suitp1);
+
+            //read so the consol doesnt close. ghetto fix but works c:
+            Console.Read();
         }
 
+        // one of the sort methods we used in levacs class
         public static void sortcards(int[] A)
         {
+
             for (int n = 1; n <= A.Length - 1; n++)
             {
                 int temp = A[n];
@@ -48,51 +54,73 @@ namespace crazy_8s
             }
         }
 
-        private static void handoutcards(int[][] cards,int[] player,string []suit)
+
+        //
+        private static void handoutcards(int[][] cards, int[] player, string[] suit)
         {
+
             Random random = new Random();
             for (int i = 0; i < player.Length; i++)
             {
+
+                //create two random intigers storing them in variables
                 int temp = random.Next(4);
                 int temp2 = random.Next(13);
+
+                // for use in while loop
                 bool handed = false;
 
-                while (!handed)
+                //while loop where the player actually gets his card
+                while (handed==false)
                 {
-                    if (cards[temp][temp2] != 0)
+
+                    // 0 is the default value for a card that has been handed out. 
+                    if (!(cards[temp][temp2] == 0))
                     {
 
+                        player[i] = cards[temp][temp2];
+                        cards[temp][temp2] = 0;
+                        
+                        
+                        // choose the suit of the card
                         if (temp == 0)
-                        {
-                            suit[i] = "hearts";
-                        }
-                        else if (temp == 1)
-                        {
-                            suit[i] = "diamonds";
-                        }
-                        else if (temp == 2)
-                        {
-                            suit[i] = "clubs";
-                        }
-                        else if (temp == 3)
                         {
                             suit[i] = "spades";
                         }
-                        player[i] = cards[temp][temp2];
-                        cards[temp][temp2] = 0;
+                        else if (temp == 1)
+                        {
+                            suit[i] = "clubs";
+                        }
+                        else if (temp == 2)
+                        {
+                            suit[i] = "diamonds";
+                        }
+                        else if (temp == 3)
+                        {
+                            suit[i] = "hearts";
+                        }
+
                         handed = true;
+                        
                     }
-                }
+                    else if (cards[temp][temp2]==0)
+                    {
+                        // if the card the randoms chose is used, re roll duh card. niiiiigaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+                        temp = random.Next(4);
+                        temp2 = random.Next(13);
+                        Console.WriteLine("pooooop on s stick BOIS");
+                    }
+                }///while loop
             }///forloop
 
 
         }///handoutcards function
 
-        private static void outputcards(int[] player,string[]suit)
+        private static void outputcards(int[] player, string[] suit)
         {
             for (int i = 0; i < player1.Length; i++)
             {
-
+                //if the card number is one, say it as ace,if its 11 its jack etc etc. 
                 if (player[i] == 1)
                 {
                     Console.WriteLine("ace of " + suit[i]);
@@ -114,7 +142,7 @@ namespace crazy_8s
                     Console.WriteLine(player[i] + " of " + suit[i]);
                 }
             }
-            Console.Read();
+
         }///outputcards function
     }///program class
 }///namespace
